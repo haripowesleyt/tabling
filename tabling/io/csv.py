3c37fb8ea354b6ec7fd5a27871b1e60c108242ed
= <=>
2025-06-03 03:35:17 +0200

    feat: add newline= to opening csv/tsv files

--- tabling/io/csv.py
+++ tabling/io/csv.py
@@ -10,7 +10,7 @@ class csv:  # pylint: disable=invalid-name
     @staticmethod
     def dump(table: Table, filepath: str) -> None:
         """Dumps a table to a CSV file."""
-        with open(filepath, "w", encoding="utf-8") as csv_file:
+        with open(filepath, "w", encoding="utf-8", newline="") as csv_file:
             csv_writer = writer(csv_file)
             for row in table:
                 csv_writer.writerow((f"{cell.value}" for cell in row))
@@ -18,6 +18,6 @@ class csv:  # pylint: disable=invalid-name
     @staticmethod
     def load(table: Table, filepath: str) -> None:
         """Loads rows from a CSV file to a table."""
-        with open(filepath, "r", encoding="utf-8") as csv_file:
+        with open(filepath, "r", encoding="utf-8", newline="") as csv_file:
             for entries in reader(csv_file):
                 table.add_row(entries)
--- tabling/io/tsv.py
+++ tabling/io/tsv.py
@@ -10,7 +10,7 @@ class tsv:  # pylint: disable=invalid-name
     @staticmethod
     def dump(table: Table, filepath: str) -> None:
         """Dumps a table to a TSV file."""
-        with open(filepath, "w", encoding="utf-8") as tsv_file:
+        with open(filepath, "w", encoding="utf-8", newline="") as tsv_file:
             tsv_writer = writer(tsv_file, delimiter="\t")
             for row in table:
                 tsv_writer.writerow((f"{cell.value}" for cell in row))
@@ -18,6 +18,6 @@ class tsv:  # pylint: disable=invalid-name
     @staticmethod
     def load(table: Table, filepath: str) -> None:
         """Loads rows from a TSV file to a table."""
-        with open(filepath, "r", encoding="utf-8") as tsv_file:
+        with open(filepath, "r", encoding="utf-8", newline="") as tsv_file:
             for entries in reader(tsv_file, delimiter="\t"):
                 table.add_row(entries)
